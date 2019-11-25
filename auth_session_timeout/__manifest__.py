8bd8502ca84f5eece403c643fcd7017907e774a5
Amit Jindal <amitjindal@aquevix.com>
2019-11-25 18:53:05 +0530

    Setting custom values so we can recognize this package
    Signed-off-by: Amit Jindal <amitjindal@aquevix.com>

--- auth_session_timeout/__manifest__.py
+++ auth_session_timeout/__manifest__.py
@@ -2,18 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Inactive Sessions Timeout",
+    'name': "Internal Users - Inactive Sessions Timeout",
     'summary': """
         This module disable all inactive sessions since a given delay""",
     'author': "ACSONE SA/NV, "
               "Dhinesh D, "
               "Jesse Morgan, "
               "LasLabs, "
-              "Odoo Community Association (OCA)",
-    'maintainer': 'Odoo Community Association (OCA)',
-    'website': "http://www.github.com/OCA/server-auth",
+              "Odoo Community Association (OCA)"
+              "Aquevix Solutions Pvt Ltd.",
+    'maintainer': 'Aquevix Solutions Pvt Ltd.',
+    'website': "http://www.github.com/Aquevix/server-auth",
     'category': 'Tools',
-    'version': '12.0.1.0.0',
+    'version': '12.1.0.0.0',
     'license': 'AGPL-3',
     'data': [
         'data/ir_config_parameter_data.xml'
